bbd67cf7de09a2b099c51e9741154ce90bc54511
Ivan Orlov <orlov_mail@mail.ru>
2015-10-25 11:54:41 +0300

    widget class

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# yii2-safe-text
-Hiding personal data(email, phone, etc.) from spam grabbing
+Yii2-safe-text
+===================
+Yii2 extension for hiding personal data(email, phone, etc.) from spam grabbing
+
+Installation
+---
+Run
+```code
+php composer.phar require "demi/safe-text" "~1.0"
+```
+or
+
+
+Add to composer.json in your project
+```json
+{
+	"require": {
+  		"demi/safe-text": "~1.0"
+	}
+}
+```
+then run command
+```code
+php composer.phar update
+```
+
+# Usage
+---
+
+### Hide phone number:
+```php
+<?= \demi\safeText\Widget::widget(['text' => '+555 478 24 75']) ?>
+```
+will be generated:
+```javascript
+document.write(["5","7"," ","4","2"," ","8","7","4"," ","5","5","5","+"].reverse().join(''));
+```
+
+### Hide email:
+```php
+<?= \demi\safeText\Widget::widget([
+    'url' => 'mailto:example@gmail.com',
+    'text' => 'example@gmail.com',
+]) ?>
+```
+will be generated:
+
+```javascript
+(function() {
+    var a = document.createElement('a');
+    a.href = ["m","o","c",".","l","i","a","m","g","@","e","l","p","m","a","x","e",":","o","t","l","i","a","m"].reverse().join('');
+    var text = ["m","o","c",".","l","i","a","m","g","@","e","l","p","m","a","x","e"].reverse().join('');
+    var template = '{text}';
+    a.innerHTML = template.replace('{text}', text);
+
+    document.write(a.outerHTML);
+}());
+```
+
+# Options
+---
+All widget options
+```php
+<?= \demi\safeText\Widget::widget([
+    'url' => 'skype:girlfriend?chat',
+    'text' => 'girlfriend',
+    'linkTextTemplate' => '<i class="fa fa-skype"></i> {text}',
+    'linkOptions' => [
+        'class' => 'skype-link',
+        'target' => '_blank',
+    ],
+]) ?>
+```
+
+will be generate:
+```javascript
+(function() {
+    var a = document.createElement('a');
+    a.href = ["t","a","h","c","?","d","n","e","i","r","f","l","r","i","g",":","e","p","y","k","s"].reverse().join('');
+    a.setAttribute('class', 'skype-link');
+    a.setAttribute('target', '_blank');
+    var text = ["d","n","e","i","r","f","l","r","i","g"].reverse().join('');
+    var template = '<i class="fa fa-skype"></i> {text}';
+    a.innerHTML = template.replace('{text}', text);
+
+    document.write(a.outerHTML);
+}());
+```
---
+++ Widget.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace demi\safeText;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\Url;
+
+/**
+ * Widget for safe output text
+ *
+ * hide phone number:
+ * ```php
+ *
+ * ```
+ *
+ * @package demi\safeText
+ */
+class Widget extends \yii\base\Widget
+{
+    /**
+     * Text for safing
+     *
+     * @var string
+     */
+    public $text;
+    /**
+     * Url for text if needeed. Link will also be safe
+     *
+     * @var array|string|false
+     */
+    public $url = false;
+    /**
+     * JavaScript attributes for <a> tag.
+     * for example: ['class' => 'testClass']
+     *
+     * @var array
+     */
+    public $linkOptions = [];
+    /**
+     * Text template. You can insert any non-personal data to link text.
+     * for example: '<i class="fa fa-envelope"></i> {text}'
+     *
+     * @var string
+     */
+    public $linkTextTemplate = '{text}';
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $output = '';
+        // if text is empty
+        if (empty($this->text)) {
+            return $output;
+        }
+
+        $encoded = self::encode($this->text);
+        $js_chars = Json::encode($encoded);
+
+        if ($this->url === false || is_null($this->url)) {
+            $script = "document.write($js_chars.reverse().join(''));";
+        } else {
+            $linkAttributes = '';
+
+            foreach ($this->linkOptions as $k => $v) {
+                $linkAttributes .= PHP_EOL . "a.setAttribute('$k', '$v');";
+            }
+
+            $url_chars = Json::encode(self::encode(Url::to($this->url)));
+            $script = "
+			(function() {
+				var a = document.createElement('a');
+				a.href = $url_chars.reverse().join('');$linkAttributes
+				var text = $js_chars.reverse().join('');
+				var template = '$this->linkTextTemplate';
+				a.innerHTML = template.replace('{text}', text);
+
+				document.write(a.outerHTML);
+			}());";
+        }
+        $output .= Html::script($script) . PHP_EOL;
+
+        return $output;
+    }
+
+    /**
+     * Encode unsafe string
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    public static function encode($text)
+    {
+        // Splitting a string into individual characters
+        $chars = preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY);
+
+        // Expand string in reverse order
+        return array_reverse($chars);
+    }
+
+    /**
+     * Decode safe string
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    public static function decode($text)
+    {
+        // Splitting a string into individual characters
+        $chars = preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY);
+
+        // Expand string in reverse order
+        return array_reverse($chars);
+    }
+}
